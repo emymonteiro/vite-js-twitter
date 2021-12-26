47787c74735079db045821dc83b667610f7df9e8
Emilly <emymonteiro.sweetie@gmail.com>
2021-12-25 23:13:10 -0400

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <div class="min-h-screen bg-emerald-400 dark:bg-black text-white" >
+      <h1>Hello World</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
